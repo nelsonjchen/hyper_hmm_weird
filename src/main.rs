922d367a0f4838f0361049447c2366f67a951c64
Nelson Chen <crazysim@gmail.com>
2015-09-06 15:19:09 -0700

    early eof

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,15 @@ fn main() {
     let mut client = Client::new();
 
     // Creating an outgoing request.
-    let mut res = client.get("http://lsanca-speedtest-01-a.socal.rr.com/speedtest/latency.txt")
+    let mut res = client.get("http://lsanca-speedtest-01-a.socal.rr.com/speedtest/random4000x4000.jpg")
         // set a header
         .header(Connection::close())
         // let 'er go!
         .send().unwrap();
 
     // Read the Response.
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
+    let mut body: Vec<u8>= vec!();
+    res.read(&mut body).unwrap();
 
-    println!("Response: {}", body);
+    println!("Response: {:?}", body);
 }
