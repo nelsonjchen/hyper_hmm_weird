e2733b111102a5bdc6dd24443aab2244ab6c79d0
Nelson Chen <crazysim@gmail.com>
2015-09-06 21:42:42 -0700

    Now I have *no* idea what's happening

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,32 @@ use hyper::header::{Connection, UserAgent};
 
 fn main() {
     // Create a client.
-    let client = Client::new();
 
     // Creating an outgoing request.
-    let mut res = client.get("http://lsanca-speedtest-01-a.socal.rr.com/")
-        // set a header
-        .header(UserAgent("hyper/hmmweird 0.01".to_owned()))
-        .header(Connection::close())
-        // let 'er go!
-        .send().unwrap();
+    // http://lsanca-speedtest-01-a.socal.rr.com/speedtest/latency.txt # FAIL
+    // http://speedtest.lax2.hostduplex.com/speedtest/latency.txt # SUCCESS
+    let urls = vec!(
+        "http://lsanca-speedtest-01-a.socal.rr.com/speedtest/latency.txts",
+        "http://speedtest.lax2.hostduplex.com/speedtest/latency.txt",
+        "http://speedtest.lax.hugeserver.com/speedtest/latency.txt",
+        "http://speedtest.lax.gigenet.com/speedtest/latency.txt",
+        "http://ndt.dhspeedtest.com/flash/speedtest/latency.txt",
+        "http://google.com",
+        "http://gamefaqs.com",
+        );
+    for url in urls.iter() {
+        let client = Client::new();
+        let mut res = client.get(*url)
+            // set a header
+            .header(UserAgent("hyper/hmmweird 0.01".to_owned()))
+            .header(Connection::close())
+            // let 'er go!
+            .send().unwrap();
 
-    // Read the Response.
-    let mut body: Vec<u8> = vec!();
-    let read_res = res.read(&mut body);
+        // Read the Response.
+        let mut body: Vec<u8> = vec!();
+        let read_res = res.read(&mut body);
 
-    println!("Read Result: {:?}", read_res);
+        println!("Read Result for {}: {:?}", url, read_res);
+    }
 }
