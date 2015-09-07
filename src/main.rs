50d874e5491cc1148435c94759169a084f9f2bff
Nelson Chen <crazysim@gmail.com>
2015-09-06 20:38:18 -0700

    Simpler

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,16 @@ fn main() {
     let client = Client::new();
 
     // Creating an outgoing request.
-    let mut res = client.get("http://lsanca-speedtest-01-a.socal.rr.com/speedtest/random4000x4000.jpg")
+    let mut res = client.get("http://lsanca-speedtest-01-a.socal.rr.com/")
         // set a header
-        .header(UserAgent("hyper/speedtest-rust 0.01".to_owned()))
+        .header(UserAgent("hyper/hmmweird 0.01".to_owned()))
         .header(Connection::close())
         // let 'er go!
         .send().unwrap();
 
     // Read the Response.
     let mut body: Vec<u8> = vec!();
-    res.read(&mut body).unwrap();
+    let read_res = res.read(&mut body);
 
-    println!("Response: {:?}", body);
+    println!("Read Result: {:?}", read_res);
 }
